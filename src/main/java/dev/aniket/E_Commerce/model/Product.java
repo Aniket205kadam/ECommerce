468a7a2e2cdfffbcc4b52f11401a0d4421da971f
Aniket205kadam <aniketrkadam205@gmail.com>
2024-06-25 21:26:12 +0530

    create product model

---
+++ src/main/java/dev/aniket/E_Commerce/model/Product.java
@@ -0,0 +1,27 @@
+package dev.aniket.E_Commerce.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    private Integer id;
+    private String name;
+    private String description;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+    private Date releaseDate;
+    private Boolean available;
+    private Integer quantity;
+}
